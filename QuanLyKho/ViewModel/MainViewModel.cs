4ed0dfd58a4796be0560be75dbcc5d1b3dda36a0
manhdc6 <manhdc612@gmail.com>
2025-03-06 20:46:24 +0700

    Initial Commit - Upload Project

---
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded { get; set; } = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitCommand { get; set; }
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            });
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UnitWindow wd = new UnitWindow();
+                wd.ShowDialog();
+            });
+
+        }
+    }
+}
